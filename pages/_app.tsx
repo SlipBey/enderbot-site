f705b375eb02d967b07551995eef5d0731dccb75
SlipBey <52493196+SlipBey@users.noreply.github.com>
2022-06-20 10:26:59 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,12 +35,12 @@ const App = ({ Component, pageProps }) => {
 				<meta name="HandheldFriendly" content="true" />
 				<meta
 					name="copyright"
-					content="Barış DEMİRCİ, demirci.baris38@gmail.com"
+					content="Berkant GÜNAYDIN, gunaydinberkant13@gmail.com"
 				/>
 				<meta name="theme-color" content={CONFIG.SEO.themeColor} />
 				<meta
 					name="author"
-					content="Barış DEMİRCİ, demirci.baris38@gmail.com"
+					content="Berkant GÜNAYDIN, gunaydinberkant13@gmail.com"
 				/>
 				<meta name="keywords" content={CONFIG.SEO.keywords.join(",")} />
 				<meta name="description" content={CONFIG.SEO.description} />
